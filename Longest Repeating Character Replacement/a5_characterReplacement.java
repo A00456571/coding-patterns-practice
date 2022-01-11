14d59222ddf6f686f8b156abf69b21fbdd74a05d
Manjot Bains <90558762+A00456571@users.noreply.github.com>
2022-01-10 20:28:27 -0400

    used ++count operator

---
+++ Longest Repeating Character Replacement/a5_characterReplacement.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        if(s.length()==1)
+            return 1;
+        int start = 0;
+        int[] fq = new int[26];
+        int max = 0;
+        int repeat = 0;
+        char[] ch = s.toCharArray();
+        for(int end =0;end<s.length();end++)
+        {
+            //fq[ch[end]-'A']++;
+            repeat = Math.max(repeat,++fq[ch[end]-'A']);
+            if(end-start+1-repeat>k)
+            {
+                fq[ch[start]-'A']--;
+                start++;
+            }
+            else
+                max = Math.max(max,end-start+1);
+        }
+        return max;
+    }
+}
